d201a233099a2c9134b5d921db317f106f0953da
yuraviscnhevski-dev <yura.viscnhevski@gmail.com .>
2026-03-16 15:13:17 +0300

    I'm Groot!

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -1,21 +1,29 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
+import ru.kata.spring.boot_security.demo.models.Role;
 import ru.kata.spring.boot_security.demo.models.User;
 import ru.kata.spring.boot_security.demo.services.UserService;
+import ru.kata.spring.boot_security.demo.repositories.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
     private final UserService userService;
+    private final RoleRepository roleRepository;
 
     @Autowired
-    public AdminController(UserService userService) {
+    public AdminController(UserService userService, RoleRepository roleRepository) {
         this.userService = userService;
+        this.roleRepository = roleRepository;
     }
 
     @GetMapping
@@ -27,11 +35,17 @@ public class AdminController {
     @GetMapping("/new")
     public String newUser(Model model) {
         model.addAttribute("user", new User());
+        model.addAttribute("allRoles", roleRepository.findAll()); // все роли для выбора
         return "new";
     }
 
     @PostMapping("/new")
-    public String createUser(@ModelAttribute("user") User user) {
+    public String createUser(@ModelAttribute("user") User user,
+                             @RequestParam(value = "roleIds", required = false) List<Long> roleIds) {
+        if (roleIds != null && !roleIds.isEmpty()) {
+            Set<Role> roles = new HashSet<>(roleRepository.findAllById(roleIds));
+            user.setRoles(roles);
+        }
         userService.save(user);
         return "redirect:/admin";
     }
@@ -39,12 +53,21 @@ public class AdminController {
     @GetMapping("/edit/{id}")
     public String editUser(@PathVariable("id") Long id, Model model) {
         model.addAttribute("user", userService.findById(id));
+        model.addAttribute("allRoles", roleRepository.findAll()); // все роли для выбора
         return "edit";
     }
 
     @PostMapping("/edit/{id}")
     public String updateUser(@PathVariable("id") Long id,
-                             @ModelAttribute("user") User user) {
+                             @ModelAttribute("user") User user,
+                             @RequestParam(value = "roleIds", required = false) List<Long> roleIds) {
+        if (roleIds != null && !roleIds.isEmpty()) {
+            Set<Role> roles = new HashSet<>(roleRepository.findAllById(roleIds));
+            user.setRoles(roles);
+        } else {
+            // Если ни одна роль не выбрана, можно либо ничего не менять, либо установить пустую коллекцию
+            user.setRoles(new HashSet<>());
+        }
         userService.update(id, user);
         return "redirect:/admin";
     }
